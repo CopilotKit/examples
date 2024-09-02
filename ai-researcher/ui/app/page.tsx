efce68b99ad9e25ada8f32cbb08a67a3aa0be92b
Ariel Weinberger <weinberger.ariel@gmail.com>
2024-09-02 16:46:40 +0200

    fix vercel build

--- ai-researcher/ui/app/page.tsx
+++ ai-researcher/ui/app/page.tsx
@@ -1,11 +1,8 @@
 "use client";
 
-import { HomeView } from "@/components/HomeView";
-import { ResultsView } from "@/components/ResultsView";
-import { ResearchProvider, useResearchContext } from "@/lib/research-provider";
-import { AnimatePresence } from "framer-motion";
+import { ResearchWrapper } from "@/components/ResearchWrapper";
+import { ResearchProvider } from "@/lib/research-provider";
 import { CopilotKit } from "@copilotkit/react-core";
-import { CopilotPopup } from "@copilotkit/react-ui";
 import "@copilotkit/react-ui/styles.css";
 
 export default function Home() {
@@ -23,40 +20,3 @@ export default function Home() {
   );
 }
 
-export function ResearchWrapper() {
-  const { researchQuery, setResearchInput } = useResearchContext();
-
-  return (
-    <>
-      <div className="flex flex-col items-center justify-center h-screen relative z-10">
-        <div className="flex-1">
-          {researchQuery ? (
-            <AnimatePresence
-              key="results"
-              onExitComplete={() => {
-                setResearchInput("");
-              }}
-              mode="wait"
-            >
-              <ResultsView key="results" />
-            </AnimatePresence>
-          ) : (
-            <AnimatePresence key="home" mode="wait">
-              <HomeView key="home" />
-            </AnimatePresence>
-          )}
-        </div>
-        <footer className="text-xs p-2">
-          <a
-            href="https://copilotkit.ai"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-slate-600 font-medium hover:underline"
-          >
-            Powered by CopilotKit 🪁
-          </a>
-        </footer>
-      </div>
-    </>
-  );
-}
